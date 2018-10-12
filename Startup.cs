5a13ba3f922d5f2847d78181b39ad6d140674823
Xsitsu <jacoblocke21@live.com>
2018-10-11 18:03:03 -0700

    Created basic project file.

--- Startup.cs
+++ Startup.cs
@@ -20,6 +20,8 @@ namespace Lab3
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
