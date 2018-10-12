3db5e2d1b8c968c331e565168d861c16d9851599
Xsitsu <jacoblocke21@live.com>
2018-10-11 18:33:41 -0700

    Added armadillo section.

---
+++ Controllers/ArmadilloController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using ArmadilloLib;
+
+namespace Lab3.Controllers
+{
+    public class ArmadilloController : Controller
+    {
+        private Random rand = new Random();
+
+        private string[] armadilloNames =
+        {
+            "Thaoztitl",
+            "V'elth'thax",
+            "Thictoxr",
+            "Grodrri",
+            "Dioll'rorh",
+            "Ynuld'endess",
+            "Uctaioghal",
+            "Ainodrux",
+            "Engyjharc",
+            "Ukhebrerh",
+            "Ngervi",
+            "Mhaojhust",
+            "Vathleth",
+            "Shoudhi",
+            "Kopex",
+            "Umliognnast",
+            "Ishoudri",
+            "Ezuthr'zha",
+            "Uxughi",
+            "Ukthouggd'ithuh"
+        };
+        private ArmadilloFarm farm = new ArmadilloFarm();
+        public ArmadilloController()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string name = armadilloNames[rand.Next(1, armadilloNames.Length)];
+                int age = rand.Next(2, 7);
+                int shellHardness = (int)(age * 1.4) + rand.Next(1, 4);
+                bool isPainted = ((age + shellHardness) % 4 == 0);
+
+                farm.FarmAnimals.Add(new Armadillo() { Name = name, Age = age, ShellHardness = shellHardness, IsPainted = isPainted });
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        IEnumerable[Armadillo]
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -15,11 +15,13 @@ namespace Lab3
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvcWithDefaultRoute();
             app.UseStaticFiles();
 
             if (env.IsDevelopment())
@@ -27,10 +29,12 @@ namespace Lab3
                 app.UseDeveloperExceptionPage();
             }
 
+            /*
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+            */
         }
     }
 }
